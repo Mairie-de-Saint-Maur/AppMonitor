8fd40be26777b07992a3df14aa8a81cf473184c9
Camus LEJARRE <camus.lejarre@mairie-saint-maur.com>
2018-05-18 14:55:07 +0200

    - nouveau scénario GRU Front

--- testcases/GRUFront.php
+++ testcases/GRUFront.php
@@ -37,74 +37,34 @@ class GRUFront extends scenario {
 
 		// Ouverture de la page d'accueil de l'application
 		$this->driver->get('http://capdemat-front-prp.multimediabs.com/connexion');
-		/*
-		// Vérification de la présence du formulaire
-		$this->driver->wait()->until(Facebook\WebDriver\WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("a[href^='http://capdemat-front-prp.multimediabs.com/connexion'")));
-		
-		//On rentre dans la frame login:
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("frame[name=login]")));
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=login]")));
-		//Puis dans la frame general
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("frame[name=general]")));
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=general]")));
 		
 		// Saisie du login et du mot de passe puis validation
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::name('login')))->clear();
-		$this->driver->findElement(WebDriverBy::name('login'))->sendKeys("alt.r2-9ok7tvlt@yopmail.com");
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::name('password')))->clear();
-		$this->driver->findElement(WebDriverBy::name('password'))->sendKeys("CLgru94100");
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::id('user_login')))->clear();
+		$this->driver->findElement(WebDriverBy::id('user_login'))->sendKeys("alt.r2-9ok7tvlt@yopmail.com");
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::id('user_pass')))->clear();
+		$this->driver->findElement(WebDriverBy::id('user_pass'))->sendKeys("CLgru94100");
 		
 		$this->driver->findElement(WebDriverBy::cssSelector("input[type=submit]"))->click();
 
 		//Vérification du chargement de la page
-		$this->driver->switchTo()->defaultContent();
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=login]")));
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("frame[name=bandeauHaut]")));
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=bandeauHaut]")));
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('.bandeauuser')));
-		*/
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('#widget-compte')));
+		
 	}
    
 	public function Action() {
-		/*
-		//parent::Action();
-		
-		//Navigation dans les frames jusqu'au lien
-		$this->driver->switchTo()->defaultContent();
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=work]")));
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("frame[name=menu]")));
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=menu]")));
-		
-		// clic sur lien "Réception"
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("form[name=loginForm] td.menuLnk a[href='javascript:f_redirectSeancesList();']")))->click();
-		
-		//Vérification du chargement
-		//Navigation dans les frames jusqu'à l'élément
-		$this->driver->switchTo()->defaultContent();
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=login]")));
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("frame[name=work]")));
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=work]")));
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("frame[name=main]")));
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=main]")));
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("frame[name=action]")));
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=action]")));
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector(".actionBtn")));*/
+		//Clic sur le lien de changement d'adresse et vérification
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::linkText("Modifier mon adresse mail")))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('#email')));
+		//Clic sur le lien de changement de mot de passe et vérification
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::linkText("Modifier mon mot de passe")))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('#pwd')));
 	}
 
 	public function Logout() {
-		/*
-		//parent::Logout();
-
-		//Navigation dans les frames jusqu'au lien
-		$this->driver->switchTo()->defaultContent();
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=login]")));
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("frame[name=work]")));
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=work]")));
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("frame[name=menu]")));
-		$this->driver->switchTo()->frame($this->driver->findElement(WebDriverBy::cssSelector("frame[name=menu]")));
-		// Déconnexion -> On trouve l'élément dont le lien commence par ...
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("a[href^='javascript:top.login.bandeauHaut.setDeconnection']")))->click();
-		*/
+		//Clic sur le bouton déconnexion et vérification
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::linkText("Déconnexion")))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('#layer_slider_1')));
+		
 	}
 }
 ?>
