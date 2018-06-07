0157ab45151a3515fedfce80e6222563a390e3d7
Camus LEJARRE <camus.lejarre@mairie-saint-maur.com>
2018-06-07 15:46:31 +0200

    - scénarion Keepeek fonctionnel

--- testcases/Keepeek.php
+++ testcases/Keepeek.php
@@ -36,37 +36,41 @@ class Keepeek extends scenario {
 
 	public function Login() {
 		// Saisie du login et du mot de passe puis validation
-		$this->driver->findElement(WebDriverBy::name('userId'))->clear();
-		$this->driver->findElement(WebDriverBy::name('userId'))->sendKeys("tech");
-		$this->driver->findElement(WebDriverBy::name('password'))->clear();
-		$this->driver->findElement(WebDriverBy::name('password'))->sendKeys("saintmaur2015?");
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::name('userId')))->clear();
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::name('userId')))->sendKeys("tech");
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::name('password')))->clear();
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::name('password')))->sendKeys("saintmaur2015?");
 		
+		//Bouton connexion "s'identifier"
 		current($this->driver->findElements(WebDriverBy::cssSelector(".x-btn-center")))->click();
 		
-		//Chargement de la page d'accueil
-		//On vérifie si une popup ne s'est pas ouverte parce que l'utilisateur est resté connecté
-		$this->driver->findElement(WebDriverBy::xpath("//button[text()[contains(.,'Se connecter')]]"));
+		//On attend, puis on vérifie si une popup ne s'est pas ouverte parce que l'utilisateur est resté connecté
+		sleep(2);
+		$el = $this->driver->findElements(WebDriverBy::xpath("//button[text()[contains(.,'connecter')]]"));
+		if (count($el) >= 1) {
+			current($el)->click();
+		}
 		
-		exit;
+		//Affichage de la page d'accueil ?
+		$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::cssSelector("img[src='http://asset.keepeek.com/permalinks/domain1009/2013/03/25/24-3-Baobab-home.jpg']")));
 		
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector("img[src='http://asset.keepeek.com/permalinks/domain1009/2013/03/25/24-3-Baobab-home.jpg']")));
-		exit;
 	}
    
 	public function Action() {
 		//Fermeture / ouverture du panneau latéral "plan du site"
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::id("ametys-ribbon-button-2005")))->click();
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::id("ametys-ribbon-button-2005")))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::xpath("//button[text()[contains(.,'Paniers')]]")))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::xpath("//button[text()[contains(.,'Utilisateurs')]]")))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::xpath("//button[text()[contains(.,'Bureau')]]")))->click();
 		
 		//Rechargement de la page
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::id("ametys-ribbon-button-2506")))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::cssSelector("img[src='http://asset.keepeek.com/permalinks/domain1009/2013/03/25/24-3-Baobab-home.jpg']")));
 	}
 
 	public function Logout() {
 		// clic sur le menu utilisateur
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::id("button-5823")))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::xpath("//button[text()[contains(.,'Administrateur Technique')]]")))->click();
 		// Déconnexion
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::id("menuitem-5695-itemEl")))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::linkText("Déconnexion")))->click();
 	}
 }
 ?>
