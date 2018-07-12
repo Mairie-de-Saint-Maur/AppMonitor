d6e7ecb83bf5ffb3d60e4569eb3caf3c48a0ded4
Hugo STEPHAN <hugo.stephan@etu.u-pec.fr>
2018-07-12 21:52:28 +0200

    Changement condition d'attente

--- testcases/Gitlab.php
+++ testcases/Gitlab.php
@@ -58,8 +58,8 @@ class Gitlab extends scenario {
 
 	public function Logout() {
 		// Déconnexion par le menu déroulant et Sign out
-		$this->driver->wait()->until(WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('a.header-user-dropdown-toggle')))->click();
-		$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOf(WebDriverBy::partialLinkText('Sign out')))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOf($this->driver->findElement(WebDriverBy::cssSelector('a.header-user-dropdown-toggle'))))->click();
+		$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOf($this->driver->findElement(WebDriverBy::partialLinkText('Sign out'))))->click();
 	}
 }
 ?>
