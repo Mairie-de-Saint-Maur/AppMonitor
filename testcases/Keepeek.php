dc911ac9c3261ae42d0bbf574a9334a00be0bd7b
stephan-hug <hugo.stephan@mairie-saint-maur.com>
2018-07-25 10:53:18 +0200

    Correction bug keepeek

--- testcases/Keepeek.php
+++ testcases/Keepeek.php
@@ -67,11 +67,19 @@ class Keepeek extends scenario {
 	}
 
 	public function Logout() {
-		// clic sur le menu utilisateur
-		//$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::xpath("//button[text()[contains(.,'Administrateur Technique')]]")))->click();
-		//$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::cssSelector("table.kpk-top-bar-user tbody tr td.x-btn-center button")))->click();
-		// Déconnexion
-		//$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::linkText("Déconnexion")))->click();
+		sleep(2);
+		$el = $this->driver->findElements(WebDriverBy::xpath("//button[text()[contains(.,'Revenir')]]"));
+		if (count($el) >= 1) {
+			current($el)->click();
+		}
+		else
+		{
+			// clic sur le menu utilisateur
+			//$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::xpath("//button[text()[contains(.,'Administrateur Technique')]]")))->click();
+			$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::cssSelector("table.kpk-top-bar-user tbody tr td.x-btn-center button")))->click();
+			// Déconnexion
+			$this->driver->wait()->until(WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::linkText("Déconnexion")))->click();
+		}
 	}
 }
 ?>
